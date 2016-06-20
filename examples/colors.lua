ca7a75bdcba31dcdd84342bf2163381e8ea65668
N-1-c-k-e <n1cke@mail.ru>
2016-06-21 00:52:21 +0300

    improved colors example

--- examples/colors.lua
+++ examples/colors.lua
@@ -1,12 +1,22 @@
 database = {}
-for i = 1, 2^16 do
-    database[i] = {bgrC = i, bgrA = 1.0}
+
+for r = 0, 255, 2 do
+	for g = 0, 255, 2 do
+		for b = 0, 255, 2 do
+			local color = 65536 * r + 256 * g + b
+			table.insert(database, {bgrC = color, bgrA = 1.0})
+		end
+	end
 end
+
 layout = Layout.new{
+	anAdd = Layout.newAnimation(),
+	anRemove = Layout.newAnimation(),
+	bringToFront = function() end,
     template = Layout, database = database,
     scroll = true, content = true,
     cellAbsW = 16, cellAbsH = 16,
-    cols = 256,
+    cols = 1024,
 }
 
 stage:addChild(layout)
